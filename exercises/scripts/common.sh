94deefc9e193632a4b370a730ab94e8771876e15
Aung Aung Myo Myint (Mr-Linuxer) <aungaungmyomyint.aa@gmail.com>
2023-06-03 03:00:15 +0800

    update2

--- exercises/scripts/common.sh
+++ exercises/scripts/common.sh
@@ -4,26 +4,16 @@ if [ -z ${K8S_VERSION+x} ]; then
   K8S_VERSION=1.23.4-00
 fi
 
-apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb  http://apt.kubernetes.io/  kubernetes-xenial  main" > /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-apt-get update
-apt-get install -y \
-  avahi-daemon \
-  libnss-mdns \
-  traceroute \
-  htop \
-  httpie \
-  bash-completion \
-  docker-ce=5:19.03.14~3-0~ubuntu-bionic \
-  kubeadm=$K8S_VERSION \
-  kubelet=$K8S_VERSION \
-  kubectl=$K8S_VERSION
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get install -y vim net-tools docker-ce avahi-daemon libnss-mdns traceroute htop httpie bash-completion kubeadm=$K8S_VERSION kubelet=$K8S_VERSION kubectl=$K8S_VERSION
+
+sudo usermod -aG docker ${USER}
+
 cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -37,8 +27,8 @@ EOF
 
 mkdir -p /etc/systemd/system/docker.service.d
 
-systemctl daemon-reload
-systemctl restart docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
 # Set alias for kubectl command
 echo "alias k=kubectl" >> /home/vagrant/.bashrc
