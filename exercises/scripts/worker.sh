cc214954a2dbc1582b0d23a49083685f30ba5f2b
Aung Aung Myo Myint (Mr-Linuxer) <aungaungmyomyint.aa@gmail.com>
2023-06-03 19:00:22 +0800

    update4

--- exercises/scripts/worker.sh
+++ exercises/scripts/worker.sh
@@ -3,9 +3,9 @@
 NODE=$1
 NODE_HOST_IP="192.168.56.$((20+$NODE))"
 
-#$(cat /vagrant/kubeadm-init.out | grep -A 2 "kubeadm join" | sed -e 's/^[ \t]*//' | tr '\n' ' ' | sed -e 's/ \\ / /g')
+$(cat /vagrant/kubeadm-init.out | grep -A 2 "kubeadm join" | sed -e 's/^[ \t]*//' | tr '\n' ' ' | sed -e 's/ \\ / /g')
 #grep -A 2 "kubeadm join" /vagrant/kubeadm-init.out | sed -e 's/^[ \t]*//' | tr '\n' ' ' | sed -e 's/ \\ / /g'
-grep -A 2 "kubeadm join" /vagrant/kubeadm-init.out > join.sh | chmod +x join.sh 
+#grep -A 2 "kubeadm join" /vagrant/kubeadm-init.out > join.sh | chmod +x join.sh 
 
 echo "KUBELET_EXTRA_ARGS=--node-ip=$NODE_HOST_IP --cgroup-driver=systemd" > /etc/default/kubelet
 systemctl restart kubelet
